8acb9f27fab7ef74d25a0a8265985e1e2cc5cd40
diiiiiiiino <63040475+diiiiiiiino@users.noreply.github.com>
2024-02-29 12:28:09 +0900

    Time: 1 ms (35.54%), Space: 42.1 MB (63.93%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -2,10 +2,6 @@ import java.util.Arrays;
 
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if(m > 0 && n == 0){
-            System.out.println(Arrays.toString(nums1));
-            return;
-        }
 
         System.arraycopy(nums2, 0, nums1, m, n);
 
