4d0db9c387a314dc3999aaa1b60aca0723c4cd8a
diiiiiiiino <63040475+diiiiiiiino@users.noreply.github.com>
2024-03-11 14:51:13 +0900

    Time: 4 ms (100.00%), Space: 43.6 MB (87.76%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -2,9 +2,6 @@ class Solution {
     public boolean isPalindrome(int x) {
         if(x < 0) return false;
         
-        Stack<Integer> stk = new Stack<>();
-        Queue<Integer> queue = new LinkedList<>();
-        
         int num = 0;
         int originX = x;
         while(x > 0){
