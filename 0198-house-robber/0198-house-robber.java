f972859cabe2ac6b7144c7a4318bfdc4f5ba96e6
diiiiiiiino <63040475+diiiiiiiino@users.noreply.github.com>
2024-03-21 12:20:23 +0900

    Time: 3 ms (100.00%), Space: 40.7 MB (87.86%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        int answer = 0;
+        int[] dy = new int[nums.length];
+
+        for(int i = 0; i < nums.length; i++){
+            dy[i] = Math.max(dy[i], nums[i]);
+            
+            for(int j = i + 2; j < nums.length; j++){
+                dy[j] = Math.max(Math.max(nums[i], dy[i]) + nums[j], dy[j]); 
+                answer = Math.max(dy[j], answer);   
+            }
+
+            answer = Math.max(answer, dy[i]);
+        }
+
+        return answer;
+    }
+}
