613f38ea00df1eee4d0d99a6a16ae337864e6cd0
diiiiiiiino <63040475+diiiiiiiino@users.noreply.github.com>
2024-05-31 15:49:14 +0900

    Time: 1 ms (94.92%), Space: 40.1 MB (94.07%) - LeetHub

--- 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.java
+++ 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.java
@@ -1,10 +1,10 @@
 class Solution {
     public int minMaxDifference(int num) {
-        String str = String.valueOf(num);
+        String maxStr = String.valueOf(num);
 
         int max = 0;
-        for(int i = 0; i < str.length(); i++){
-            int n = str.charAt(i) - '0';
+        for(int i = 0; i < maxStr.length(); i++){
+            int n = maxStr.charAt(i) - '0';
             
             if(n < 9){
                 max = n;
@@ -12,16 +12,11 @@ class Solution {
             }
         }
 
-        str = str.replace(String.valueOf(max), "9");
+        maxStr = maxStr.replace(String.valueOf(max), "9");
         
-        String str2 = String.valueOf(num);
-        int min = str2.charAt(0) - '0';
-        
-        str2 = str2.replace(String.valueOf(min), "0");
+        String minStr = String.valueOf(num);
+        minStr = minStr.replace(minStr.charAt(0), '0');
 
-        int a1 = Integer.parseInt(str);
-        int a2 = Integer.parseInt(str2);
-
-        return a1 - a2;
+        return Integer.parseInt(maxStr) - Integer.parseInt(minStr);
     }
 }
